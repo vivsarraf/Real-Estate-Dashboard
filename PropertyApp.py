cda36eecfc675634fb66b62f421c7a74515c1cd9
vivek sarraf <sarraf.vivek@gmail.com>
2024-04-18 18:02:07 -0400

    created PropertyApp to connect mongodb data to server

---
+++ PropertyApp.py
@@ -0,0 +1,34 @@
+# Import necessary libraries
+from flask import Flask, jsonify, render_template
+from flask_pymongo import PyMongo 
+import json
+from bson import json_util, regex
+import re
+
+# Creating a Flask application instance
+app = Flask(__name__)
+# Setting up the connection to a MongoDB database
+app.config["MONGO_URI"] = "mongodb://localhost:27017/home"
+mongo = PyMongo(app)
+
+# Defining a route to get all properties 
+@app.route('/')
+def index():
+    return (
+        f"/api/PropertyListings"          
+             )
+
+@app.route('/api/PropertyListings')
+def get_PropertyListings():
+    # Fetching all property information from collection in the database
+   PropertyListings = mongo.db.PropertyListings.find()
+
+    # Converting MongoDB cursor to a list
+   properties_list = list(PropertyListings)
+   
+    # Returning the list as JSON
+   return jsonify(json.loads(json_util.dumps(properties_list)))
+
+# Runing the app if this script is executed as the main program
+if __name__ == '__main__':
+    app.run(debug=True)
