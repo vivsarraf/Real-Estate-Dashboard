d4ed3ab5387bd561fff99472ae31ad6c3d89c05b
Ajunjee Selvam <ajunjee.selvam@gmail.com>
2024-04-20 12:46:42 -0400

    Completed basic visualization portion
    Completed basic visualization portion

--- Static/JS/Main.js
+++ Static/JS/Main.js
@@ -1,48 +1,41 @@
-// Creating the map object with initial settings
-let AddressMap = L.map('map', {
-  center: [43.6546, -79.3565],  // Coordinates for Toronto
-  zoom: 14                      // Initial zoom level for the map
+// Creating the map object
+let map = L.map('map', {
+  center: [43.6546, -79.3600],  // Coordinates for Toronto
+  zoom: 15                      // Initial zoom level for the map
 });
-
 
 // Adding a tile layer using OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(AddressMap);  // Adding the base layer of the map from OpenStreetMap
+}).addTo(map);  // Adding the base layer of the map from OpenStreetMap
 
-// Function to fetch attractions and add them to the map
-function fetchStations() {
-  fetch('/api/subway_stations') // Making a request to the server to get attractions data
+// Function to fetch stations and add them to the map
+function getStations() {
+  fetch('/api/subway_stations') // Making a request to the server to get stations data
     .then(response => response.json()) // Parses the response as JSON
     .then(data => {
-        addMarkersToMap(data); // Calling function to add markers based on this data
+        addMarkers(data); // Calling function to add markers based on this data
     })
     .catch(error => console.error('Error fetching data:', error)); // Logs errors, if any
 }
 
-// Function to add markers to the map using GeoJSON data
-function addMarkersToMap(data) {
-  L.geoJSON(data, {
-    onEachFeature: function (feature, layer) {
-       //  function to call on each feature before adding it to the map
-      if (feature.geometry && feature.geometry.coordinates[0]) {
-        // Extracting the latitude and longitude from the GeoJSON coordinates
-        const [longitude, latitude] = feature.geometry.coordinates[0];
-        // Binding a popup with the attraction details
-        layer.bindPopup(getPopupContent(feature.properties));
-      }
-    }
-  }).addTo(AddressMap);
+// // Function to add markers to map
+function addMarkers(data) {
+  console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      let station = data[i];
+      console.log[station];
+      let latitude = station.LAT; 
+      let longitude = station.LON; 
+        // let latitude = station.Coordinates[0]; 
+        // let longitude = station.Coordinates[1]; 
+        // let coordinates = station.Coordinates;
+      let marker = L.marker([latitude, longitude]).addTo(map);
+      marker.bindPopup("<b>" + "Station: " + "</b>" + station.Station + "</b>" + "<br>" + "<b>" + "Address: " + "</b>" + station.Address + "<br>" + "<b>" + "City: " + "</b>" + station.City + "</b>" + 
+      "<br>" + "<b>" + "Accessibility: " + "</b>" + station.Accessibility + "<br>" + "<b>" + "Parking Spaces: "+ "</b>" + station.Parking_Spaces + "<br>" + "<b>" + "Average Weekly Ridership: " + "</b>" + station.Ridership_Weekly_Average);
+  };
 }
 
-// Helper function to format popup content
-function getPopupContent(properties) {
-  return `<b>${properties.NAME}</b><br>
-          Address: ${properties.Address}<br>
-          Category: ${properties.CATEGORY}<br>
-          Phone: ${properties.PHONE}<br>
-          <a href="${properties.WEBSITE}" target="_blank">Website</a>`;
-}
 
-// Fetching attractions when the page load
-document.addEventListener('DOMContentLoaded', fetchStations);
+// Fetching stations when the page load
+document.addEventListener('DOMContentLoaded', getStations);
--- app.py
+++ app.py
@@ -5,28 +5,24 @@ import json
 from bson import json_util, regex
 import re
 
-
 # Creating a Flask application instance
 app = Flask(__name__)
 # Setting up the connection to a MongoDB database
 app.config["MONGO_URI"] = "mongodb://localhost:27017/real_estate_project_db"
 mongo = PyMongo(app)
 
-# Defining a route to get all properties 
+# Defining a route to load the map
 @app.route('/')
 def index():
     return render_template("index.html")
 
 @app.route('/api/subway_stations')
-def get_SubwayStations():
-    # Fetching all property information from collection in the database
-   ttc_subway_stations = mongo.db.subway_stations_info.find()
-
+def get_subwaystations():
+    stations = mongo.db.subway_stations_info.find()
     # Converting MongoDB cursor to a list
-   station_list = list(ttc_subway_stations)
-   
+    stations_list = list(stations)
     # Returning the list as JSON
-   return jsonify(json.loads(json_util.dumps(station_list)))
+    return jsonify(json.loads(json_util.dumps(stations_list)))
 
 # Runing the app if this script is executed as the main program
 if __name__ == '__main__':
